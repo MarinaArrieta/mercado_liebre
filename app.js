dd6f1e9a25e5c65109f82a3f4001e917dade8ef4
MarinaArrieta <arrietamarina12@gmail.com>
2022-01-05 19:10:18 -0300

    puerto de la variable de entorno

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
 	console.log('Servidor corriendo en puerto 3000');
 });
 
